c71ed449a207040688eaef001aabe2ddbecdc860
Hemali Patel <patelhemalim@gmail.com>
2022-07-28 13:08:05 -0500

    second commit

--- server.js
+++ server.js
@@ -1,64 +1,131 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+// const express = require('express')
+// const app = express()
+// const path = require('path')
 
-var Rollbar = require("rollbar");
-var rollbar = new Rollbar({
-  accessToken: '538776ef5848470c9560b4f69ed18ceb',
-  captureUncaught: true,
-  captureUnhandledRejections: true
+// var Rollbar = require("rollbar");
+// var rollbar = new Rollbar({
+//   accessToken: '538776ef5848470c9560b4f69ed18ceb',
+//   captureUncaught: true,
+//   captureUnhandledRejections: true
+// });
+
+// rollbar.log('Hello world!')
+
+// app.use(express.json())
+
+// const students = ['Jimmy', 'Timothy', 'Jimothy']
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/index.html'))
+// })
+
+// rollbar.info("Students was requested", students);
+//     res.status(200).send(students);
+// });
+
+// app.post("/api/students", (req, res) => {
+//     let { name } = req.body;
+
+//     const index = students.findIndex((student) => {
+//         return student === name;
+//     });
+
+//     try {
+//         if (index === -1 && name !== "") {
+//             students.push(name);
+
+//             rollbar.info("A new student was created", name)
+
+//             res.status(200).send(students);
+//         } else if (name === "") {
+//             rollbar.error("A student was posted without a name")
+
+//             res.status(400).send("You must enter a name.");
+//         } else {
+//             rollbar.critical("A student that already exists was posted", name)
+
+//             res.status(400).send("That student already exists.");
+//         }
+//     } catch (err) {
+//         console.log(err);
+//     }
+// });
+
+// app.delete("/api/students/:index", (req, res) => {
+//     const targetIndex = +req.params.index;
+
+//     students.splice(targetIndex, 1);
+//     res.status(200).send(students);
+// });
+
+// const port = process.env.PORT || 5050;
+
+// app.use(rollbar.errorHandler())
+
+// app.listen(port, () => console.log(`Server listening on ${port}`));
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+    accessToken: "538776ef5848470c9560b4f69ed18ceb", 
+    captureUncaught: true,
+    captureUnhandledRejections: true
 });
 
-rollbar.log('Hello world!')
+rollbar.log("Hello world!");
 
-app.use(express.json())
+app.use(express.json());
 
-const students = ['Jimmy', 'Timothy', 'Jimothy']
+const students = ["Jimmy", "Timothy", "Jimothy"];
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'))
-})
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/index.html"));
+});
 
-app.get('/api/students', (req, res) => {
-    rollbar.info("Students were reuested",students)
-    res.status(200).send(students)
-})
+app.get("/api/students", (req, res) => {
+    rollbar.info("Students was requested", students);
+    res.status(200).send(students);
+});
 
-app.post('/api/students', (req, res) => {
-   let {name} = req.body
+app.post("/api/students", (req, res) => {
+    let { name } = req.body;
 
-   const index = students.findIndex(student => {
-       return student === name
-   })
+    const index = students.findIndex((student) => {
+        return student === name;
+    });
 
-   try {
-       if (index === -1 && name !== '') {
-            students.push(name)
+    try {
+        if (index === -1 && name !== "") {
+            students.push(name);
 
-            rollbar.info("A ne wstudent was created",name)
-           res.status(200).send(students)
-       } else if (name === ''){
-rollbar.error("A student was posted without a name ")
+            rollbar.info("A new student was created", name)
 
-           res.status(400).send('You must enter a name.')
-       } else {
-           rollbar.critical("A student that already exists was posted",name)
-           res.status(400).send('That student already exists.')
-       }
-   } catch (err) {
-       console.log(err)
-   }
-})
+            res.status(200).send(students);
+        } else if (name === "") {
+            rollbar.error("A student was posted without a name")
 
-app.delete('/api/students/:index', (req, res) => {
-    const targetIndex = +req.params.index
-    
-    students.splice(targetIndex, 1)
-    res.status(200).send(students)
-})
+            res.status(400).send("You must enter a name.");
+        } else {
+            rollbar.critical("A student that already exists was posted", name)
 
-const port = process.env.PORT || 5050
+            res.status(400).send("That student already exists.");
+        }
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.delete("/api/students/:index", (req, res) => {
+    const targetIndex = +req.params.index;
+
+    students.splice(targetIndex, 1);
+    res.status(200).send(students);
+});
+
+const port = process.env.PORT || 5050;
 
 app.use(rollbar.errorHandler())
 
-app.listen(port, () => console.log(`Server listening on ${port}`))
+app.listen(port, () => console.log(`Server listening on ${port}`));
